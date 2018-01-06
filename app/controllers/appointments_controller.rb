72106eed44988dbf1508eabbb30c835705e0457c
Mel Rudick <melissarudick@MacBook-Air.local>
2018-01-06 17:12:50 -0600

    can add appt with calendar, no post action

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -1,12 +1,29 @@
 class AppointmentsController < ApplicationController
 
+  get "/appointments" do
+    @appointments = Appointment.all
+    erb :'users/show'
+  end
+
+  get '/appointments/new' do
+    if logged_in?
+      erb :'appointments/new'
+    else
+      redirect "/"
+    end
+  end
+
+  post '/appointments/new' do
+    if params[:appointment_type].empty?
+      @appointment = Appointment.create!(appointment_type: params[:appointment_type], date: params[:date])
+      session[:user_id] = @user.id
+      redirect "users/#{@user.id}"
+    end
+  end
+
   get "/appointments/:id" do
     @appointment = Appointment.find_by[:id]
     erb :'users/index'
-  end
-
-  post '/date' do
-
   end
 
 end
