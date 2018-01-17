142f95805e2f7074aec9032419a4c2a8c3eb346d
Mel Rudick <melissarudick@MacBook-Air.local>
2018-01-17 16:06:22 -0600

    most pages working, need to style

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -2,12 +2,21 @@ class ClientsController < ApplicationController
 
   get "/clients" do
     @clients = Client.all
-    redirect 'users/show'
+    erb :'clients/index'
   end
 
   get "/clients/new" do
-    @client = Client.create(name: params[:name], notes: params[:notes])
     erb :'clients/new'
+  end
+
+  post "/clients" do
+    @client = Client.create(name: params[:name], notes: params[:notes])
+    redirect "clients/#{@client.id}"
+  end
+
+  get "/clients/:id" do
+    @client = Client.find(params[:id])
+    erb :"clients/show"
   end
 
   get "/clients/:id/edit" do
